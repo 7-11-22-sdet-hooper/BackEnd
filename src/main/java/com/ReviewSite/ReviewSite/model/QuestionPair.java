73b7eb3a2ce6d6b0e9b1605be4001e55e9f85472
compscirunner <compscirunner@gmail.com>
2022-08-24 15:28:23 -0400

    converted json to match frontend

---
+++ src/main/java/com/ReviewSite/ReviewSite/model/QuestionPair.java
@@ -0,0 +1,11 @@
+package com.ReviewSite.ReviewSite.model;
+
+public class QuestionPair {
+    public String question;
+    public Boolean isCorrect;
+
+    public QuestionPair(String question, Boolean isCorrect) {
+        this.question = question;
+        this.isCorrect = isCorrect;
+    }
+}
